4b051b2cdb3d653192372e559c1bf3b472f570c6
Michał Korwel <michal.korwel@britenet.com.pl>
2019-03-19 17:35:21 +0100

    HashMap

--- src/main/java/com/example/testEnum/TestEnumApplication.java
+++ src/main/java/com/example/testEnum/TestEnumApplication.java
@@ -1,7 +1,6 @@
 package com.example.testEnum;
 
 import com.example.testEnum.service.MovieService;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,7 +9,8 @@ public class TestEnumApplication {
 	public static void main(String[] args) {
 		MovieService movieService = new MovieService();
 		movieService.addMovie();
-		SpringApplication.run(TestEnumApplication.class, args);
+		movieService.applyMovie();
+		//SpringApplication.run(TestEnumApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/testEnum/service/MovieService.java
+++ src/main/java/com/example/testEnum/service/MovieService.java
@@ -1,10 +1,23 @@
 package com.example.testEnum.service;
 
+import java.util.HashMap;
+
 public class MovieService {
+
+  HashMap<Integer, String> movies = new HashMap<>();
 
   @MovieInterface
   public void addMovie() {
     System.out.println("Movie added");
+  }
 
+  public void applyMovie() {
+    movies.put(1, "Avengers");
+    movies.put(2, "Avengers2");
+    movies.put(3, "Avengers3");
+    movies.put(1, "Thor");
+    for (String s: movies.values()) {
+      System.out.println(s);
+    }
   }
 }
